1ef6b65f2ed0ddd33c4bedfae57897e8f758c18a
Hiroto Kitazawa <hiroxto@gmail.com>
2024-03-08 17:24:23 +0900

    feat: `hiroxto.net` の DNS 構成を Terraform に移行

---
+++ main.tf
@@ -0,0 +1,4 @@
+module "hiroxto-net" {
+  source             = "./hiroxto-net"
+  cloudflare_zone_id = "1c68e572f5d9da0eeb2147cedd370816"
+}
---
+++ versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.7.0"
+
+  cloud {
+    organization = "hiroxto"
+    workspaces {
+      name = "cloudflare-domains"
+    }
+  }
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "4.26.0"
+    }
+  }
+}
