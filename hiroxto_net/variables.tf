a9b84ad9811c148c2e45576a1e41b569b4bd4f72
Hiroto Kitazawa <hiroxto@gmail.com>
2024-04-09 00:11:14 +0900

    feat: モジュールの変数にアカウントIDを追加

--- hiroxto_net/variables.tf
+++ hiroxto_net/variables.tf
@@ -2,3 +2,8 @@ variable "cloudflare_zone_id" {
   type        = string
   description = "Cloudflare の Zone ID"
 }
+
+variable "cloudflare_account_id" {
+  type        = string
+  description = "Cloudflare の Account ID"
+}
--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 module "hiroxto_net" {
-  source             = "./hiroxto_net"
-  cloudflare_zone_id = "1c68e572f5d9da0eeb2147cedd370816"
+  source                = "./hiroxto_net"
+  cloudflare_zone_id    = "1c68e572f5d9da0eeb2147cedd370816"
+  cloudflare_account_id = "85fae46d15f3eb163074860b692a91d0"
 }
