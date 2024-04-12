8cec2f38de7e0e4da69603c31749001987a6d625
Hiroto Kitazawa <hiroxto@gmail.com>
2024-04-13 01:26:43 +0900

    style: fix code style

--- hiroxto_net/page_rule.tf
+++ hiroxto_net/page_rule.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_page_rule" "dot_to_install_script" {
-  zone_id  = var.cloudflare_zone_id
-  target   = "dot.hiroxto.net/*"
+  zone_id = var.cloudflare_zone_id
+  target  = "dot.hiroxto.net/*"
   actions {
     forwarding_url {
       status_code = 301
