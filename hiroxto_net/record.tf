a4cd59c0a03f673564784c4e90f6c6618d7b6c89
Hiroto Kitazawa <hiroxto@gmail.com>
2024-04-16 02:10:18 +0900

    chore: zone_id を一番最初に定義するように統一

--- hiroxto_net/record.tf
+++ hiroxto_net/record.tf
@@ -1,91 +1,91 @@
 resource "cloudflare_record" "cname_root" {
+  zone_id = cloudflare_zone.main.id
   name    = cloudflare_zone.main.zone
   type    = "CNAME"
   value   = "hiroxto-net.netlify.app"
-  zone_id = cloudflare_zone.main.id
   proxied = true
   ttl     = 1
 }
 
 resource "cloudflare_record" "cname_www" {
+  zone_id = cloudflare_zone.main.id
   name    = "www"
   type    = "CNAME"
   value   = "hiroxto-net.netlify.app"
-  zone_id = cloudflare_zone.main.id
   proxied = true
   ttl     = 1
 }
 
 resource "cloudflare_record" "cname_swarm_checkin_regulation_checker" {
+  zone_id = cloudflare_zone.main.id
   name    = "swarm-checkin-regulation-checker"
   type    = "CNAME"
   value   = "swarm-checkin-regulation-checker.pages.dev"
-  zone_id = cloudflare_zone.main.id
   proxied = true
   ttl     = 1
 }
 
 resource "cloudflare_record" "cname_train_photo_blog" {
+  zone_id = cloudflare_zone.main.id
   type    = "CNAME"
   name    = "train-photo-blog"
   value   = "hiroxto-train-photo-blog.netlify.app"
-  zone_id = cloudflare_zone.main.id
   proxied = true
   ttl     = 1
 }
 
 resource "cloudflare_record" "cname_utils" {
+  zone_id = cloudflare_zone.main.id
   name    = "utils"
   type    = "CNAME"
   value   = "hiroxto-utils-hiroxto-net.pages.dev"
-  zone_id = cloudflare_zone.main.id
   proxied = true
   ttl     = 1
 }
 
 resource "cloudflare_record" "mx_1" {
+  zone_id  = cloudflare_zone.main.id
   name     = cloudflare_zone.main.zone
   type     = "MX"
   value    = "route1.mx.cloudflare.net"
-  zone_id  = cloudflare_zone.main.id
   priority = 57
 }
 
 resource "cloudflare_record" "mx_2" {
+  zone_id  = cloudflare_zone.main.id
   name     = cloudflare_zone.main.zone
   type     = "MX"
   value    = "route2.mx.cloudflare.net"
-  zone_id  = cloudflare_zone.main.id
   priority = 73
 }
 
 resource "cloudflare_record" "mx_3" {
+  zone_id  = cloudflare_zone.main.id
   name     = cloudflare_zone.main.zone
   type     = "MX"
   value    = "route3.mx.cloudflare.net"
-  zone_id  = cloudflare_zone.main.id
   priority = 29
 }
 
 resource "cloudflare_record" "txt_spf" {
+  zone_id = cloudflare_zone.main.id
   name    = cloudflare_zone.main.zone
   type    = "TXT"
   value   = "v=spf1 include:_spf.mx.cloudflare.net include:_spf.google.com ~all"
-  zone_id = cloudflare_zone.main.id
 }
 
 resource "cloudflare_record" "txt_dmarc" {
+  zone_id = cloudflare_zone.main.id
   name    = "_dmarc"
   type    = "TXT"
   value   = "v=DMARC1;  p=none; rua=mailto:97f14f78b6474427b3c6f73b9464f836@dmarc-reports.cloudflare.net"
-  zone_id = cloudflare_zone.main.id
 }
 
 resource "cloudflare_record" "txt_atproto" {
+  zone_id = cloudflare_zone.main.id
   name    = "_atproto"
   type    = "TXT"
   value   = "did=did:plc:ky336ximtcas2d34kfg3sosh"
-  zone_id = cloudflare_zone.main.id
   comment = "For Bluesky handle"
   proxied = false
   ttl     = 1
