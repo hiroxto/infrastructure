5d8edd68e3f949bdc20072ab267ddc6e2bad1b73
Hiroto Kitazawa <hiroxto@gmail.com>
2024-04-15 23:47:59 +0900

    refactor: 不要な変数を削除

--- hiroxto_net/variables.tf
+++ hiroxto_net/variables.tf
@@ -1,13 +1,3 @@
-variable "cloudflare_zone_id" {
-  type        = string
-  description = "Cloudflare の Zone ID"
-}
-
-variable "cloudflare_account_id" {
-  type        = string
-  description = "Cloudflare の Account ID"
-}
-
 variable "domain_name" {
   type        = string
   description = "設定するドメイン名"
