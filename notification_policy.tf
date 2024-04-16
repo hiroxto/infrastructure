b17911653d3f40eb6987c4857c0380d1f8ac5e64
Hiroto Kitazawa <hiroxto@gmail.com>
2024-04-16 15:11:55 +0900

    chore: スキーマの定義順を分かりやすくした

--- notification_policy.tf
+++ notification_policy.tf
@@ -1,9 +1,10 @@
 resource "cloudflare_notification_policy" "real_origin_monitoring_to_email" {
-  account_id  = cloudflare_account.main.id
+  account_id = cloudflare_account.main.id
+
+  enabled     = true
   alert_type  = "real_origin_monitoring"
   name        = "Passive Origin Monitoring"
   description = "Recieve an email when your origin becomes unreachable"
-  enabled     = true
 
   email_integration {
     id = "hiroto.ktzw@gmail.com"
@@ -11,11 +12,11 @@ resource "cloudflare_notification_policy" "real_origin_monitoring_to_email" {
 }
 
 resource "cloudflare_notification_policy" "web_analytics_metrics_update" {
-  account_id  = cloudflare_account.main.id
-  alert_type  = "web_analytics_metrics_update"
-  name        = "Update Web Analytics Metrix"
-  description = ""
-  enabled     = true
+  account_id = cloudflare_account.main.id
+
+  enabled    = true
+  alert_type = "web_analytics_metrics_update"
+  name       = "Update Web Analytics Metrix"
 
   webhooks_integration {
     id   = cloudflare_notification_policy_webhooks.slack.id
