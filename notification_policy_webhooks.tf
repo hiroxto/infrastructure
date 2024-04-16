ca90d1cfee1af29905ab6d8fbef071b212018949
Hiroto Kitazawa <hiroxto@gmail.com>
2024-04-16 21:54:58 +0900

    chore: 通知名を変更

--- notification_policy_webhooks.tf
+++ notification_policy_webhooks.tf
@@ -1,5 +1,5 @@
 resource "cloudflare_notification_policy_webhooks" "slack" {
   account_id = cloudflare_account.main.id
-  name       = "Slack(hiroxto) Cloudflare Notification"
+  name       = "Cloudflare Notification"
   url        = "https://hooks.slack.com/services/T016EEV6MU3/B06UD82EPJ6"
 }
