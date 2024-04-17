071c6af28444113201788119f76778ff62899ecb
Hiroto Kitazawa <hiroxto@gmail.com>
2024-04-18 00:29:29 +0900

    fix: locals to local

--- hiroxto_net/email_routing_rule.tf
+++ hiroxto_net/email_routing_rule.tf
@@ -21,13 +21,13 @@ resource "cloudflare_email_routing_rule" "forward_me_to_gmail" {
 
 resource "cloudflare_email_routing_rule" "forward_bot_to_gmail" {
   zone_id = cloudflare_zone.main.id
-  name    = "Forward ${locals.bot}@${cloudflare_zone.main.zone} to Gmail"
+  name    = "Forward ${local.bot}@${cloudflare_zone.main.zone} to Gmail"
   enabled = true
 
   matcher {
     type  = "literal"
     field = "to"
-    value = "${locals.bot}@${cloudflare_zone.main.zone}"
+    value = "${local.bot}@${cloudflare_zone.main.zone}"
   }
 
   action {
