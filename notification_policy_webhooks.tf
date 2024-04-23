8b746fe3b88e5876220e8b86dfecbd6fd7a6fdd1
Hiroto Kitazawa <hiroxto@gmail.com>
2024-04-24 00:52:51 +0900

    feat: Status向けのWebhookを作成

--- notification_policy_webhooks.tf
+++ notification_policy_webhooks.tf
@@ -9,3 +9,9 @@ resource "cloudflare_notification_policy_webhooks" "slack_tunnel" {
   name       = "Cloudflare Tunnel Notification"
   url        = "https://hooks.slack.com/services/T016EEV6MU3/B070B67DC3E"
 }
+
+resource "cloudflare_notification_policy_webhooks" "slack_status" {
+  account_id = cloudflare_account.main.id
+  name       = "Cloudflare Status Notification"
+  url        = "https://hooks.slack.com/services/T016EEV6MU3/B0708PFRT6E"
+}
