78acfc5cb7ea93cc2ef25c94bd77e9f2cade5b4b
Hiroto Kitazawa <hiroxto@gmail.com>
2024-04-24 00:49:54 +0900

    feat: Tunnel向けのWebhookを作成

--- notification_policy_webhooks.tf
+++ notification_policy_webhooks.tf
@@ -3,3 +3,9 @@ resource "cloudflare_notification_policy_webhooks" "slack" {
   name       = "Cloudflare Notification"
   url        = "https://hooks.slack.com/services/T016EEV6MU3/B06UD82EPJ6"
 }
+
+resource "cloudflare_notification_policy_webhooks" "slack_tunnel" {
+  account_id = cloudflare_account.main.id
+  name       = "Cloudflare Tunnel Notification"
+  url        = "https://hooks.slack.com/services/T016EEV6MU3/B070B67DC3E"
+}
