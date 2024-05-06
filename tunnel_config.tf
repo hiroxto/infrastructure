61e00bb1813dc7215cd144147de68b3c6815350a
Hiroto Kitazawa <hiroxto@gmail.com>
2024-05-07 00:20:10 +0900

    feat: Tunnelのorigin_requestのオプションを設定

--- tunnel_config.tf
+++ tunnel_config.tf
@@ -6,7 +6,11 @@ resource "cloudflare_tunnel_config" "epgstation" {
     ingress_rule {
       hostname = "epgstation.hiroxto.net"
       service  = "http://localhost:8080"
-      origin_request {}
+      origin_request {
+        connect_timeout = "1m0s"
+        tls_timeout     = "1m0s"
+        tcp_keep_alive  = "1m0s"
+      }
     }
     ingress_rule {
       service = "http_status:404"
