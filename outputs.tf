b988247c6205be7978a69a2b3d8ea14c3f84a123
Hiroto Kitazawa <hiroxto@gmail.com>
2024-05-27 23:05:44 +0900

    feat: outputs に Tunnel 関連の情報を追加

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,17 @@ output "hiroxto_net" {
     zone_id = module.hiroxto_net.zone_id
   }
 }
+
+output "tunnel_raspi_4b_01_id" {
+  value = cloudflare_tunnel.raspi_4b_01.id
+}
+
+output "tunnel_raspi_4b_01_secret" {
+  value     = cloudflare_tunnel.raspi_4b_01.secret
+  sensitive = true
+}
+
+output "tunnel_raspi_4b_01_token" {
+  value     = cloudflare_tunnel.raspi_4b_01.tunnel_token
+  sensitive = true
+}
