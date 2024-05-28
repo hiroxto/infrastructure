5a60d103a2ac56948a830d5986bcf8767b30697c
Hiroto Kitazawa <hiroxto@gmail.com>
2024-05-29 00:37:20 +0900

    feat: Cloudflare Pages への CNAME の設定に pages_project の subdomain を使う

---
+++ pages/outputs.tf
@@ -0,0 +1,7 @@
+output "hiroxto_net_subdomain" {
+  value = cloudflare_pages_project.hiroxto_net.subdomain
+}
+
+output "train_photo_blog_subdomain" {
+  value = cloudflare_pages_project.train_photo_blog.subdomain
+}
