814e7b3e878f2a347703a68774d5ae914311a5d0
Hiroto Kitazawa <hiroxto@gmail.com>
2024-06-02 01:48:55 +0900

    fix: Tunnel の Mirakurun のサービスをリバースプロキシに変更

--- zero_trust/tunnel_config.tf
+++ zero_trust/tunnel_config.tf
@@ -14,7 +14,7 @@ resource "cloudflare_tunnel_config" "raspi_4b_01" {
     }
     ingress_rule {
       hostname = cloudflare_access_application.mirakurun.domain
-      service  = "http://127.0.0.1:40772"
+      service  = "http://127.0.0.1:8080"
       origin_request {
         connect_timeout = "1m0s"
         tls_timeout     = "1m0s"
