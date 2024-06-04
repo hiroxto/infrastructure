f9a58d7dc82e0379516c6c0e4564df1a7b46e846
Hiroto Kitazawa <hiroxto@gmail.com>
2024-06-04 23:18:07 +0900

    feat: eq12_01のTunnelと設定を追加

--- zero_trust/outputs.tf
+++ zero_trust/outputs.tf
@@ -1,17 +1,8 @@
+#
+# raspi_4b_01
+#
 output "tunnel_raspi_4b_01_id" {
   value = cloudflare_tunnel.raspi_4b_01.id
-}
-
-output "tunnel_epgstation_cname" {
-  value = cloudflare_tunnel.raspi_4b_01.cname
-}
-
-output "tunnel_mirakurun_cname" {
-  value = cloudflare_tunnel.raspi_4b_01.cname
-}
-
-output "tunnel_raspi_4b_01_cname" {
-  value = cloudflare_tunnel.raspi_4b_01.cname
 }
 
 output "tunnel_raspi_4b_01_secret" {
@@ -22,4 +13,36 @@ output "tunnel_raspi_4b_01_secret" {
 output "tunnel_raspi_4b_01_token" {
   value     = cloudflare_tunnel.raspi_4b_01.tunnel_token
   sensitive = true
+}
+
+#
+# eq12_01
+#
+output "tunnel_eq12_01_id" {
+  value = cloudflare_tunnel.eq12_01.id
+}
+
+output "tunnel_eq12_01_secret" {
+  value     = cloudflare_tunnel.eq12_01.secret
+  sensitive = true
+}
+
+output "tunnel_eq12_01_token" {
+  value     = cloudflare_tunnel.eq12_01.tunnel_token
+  sensitive = true
+}
+
+#
+# CNAME
+#
+output "tunnel_epgstation_cname" {
+  value = cloudflare_tunnel.raspi_4b_01.cname
+}
+
+output "tunnel_mirakurun_cname" {
+  value = cloudflare_tunnel.raspi_4b_01.cname
+}
+
+output "tunnel_raspi_4b_01_cname" {
+  value = cloudflare_tunnel.raspi_4b_01.cname
 }
--- zero_trust/tunnel.tf
+++ zero_trust/tunnel.tf
@@ -5,3 +5,14 @@ resource "cloudflare_tunnel" "raspi_4b_01" {
   secret     = var.tunnel_epgstation_secret
   config_src = "cloudflare"
 }
+
+resource "cloudflare_tunnel" "eq12_01" {
+  account_id = var.cloudflare_account_id
+  name       = "eq12_01"
+  secret     = base64encode(random_password.eq12_01_tunnel_secret.result)
+  config_src = "cloudflare"
+}
+
+resource "random_password" "eq12_01_tunnel_secret" {
+  length = 64
+}
