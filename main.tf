e9ebdaba31011438bed1105407217b665459e67e
Hiroto Kitazawa <hiroxto@gmail.com>
2024-06-11 00:40:45 +0900

    feat: EPGStation の Synthetics Monitor を作成

--- main.tf
+++ main.tf
@@ -55,5 +55,8 @@ module "tf_cloud" {
 # New Relic
 #
 module "observe" {
-  source = "./observe"
+  source                                          = "./observe"
+  synthetics_monitor_epgstation_uri               = "https://${module.hiroxto_net.records.cname_epgstation_hostname}/api/version"
+  synthetics_monitor_epgstation_cfa_client_id     = module.zero_trust.access_service_token_new_relic_client_id
+  synthetics_monitor_epgstation_cfa_client_secret = module.zero_trust.access_service_token_new_relic_client_secret
 }
---
+++ observe/synthetics_monitor.tf
@@ -0,0 +1,25 @@
+resource "newrelic_synthetics_monitor" "epgstation" {
+  status           = "ENABLED"
+  name             = "monitor"
+  period           = "EVERY_MINUTE"
+  uri              = var.synthetics_monitor_epgstation_uri
+  type             = "SIMPLE"
+  locations_public = ["AWS_AP_NORTHEAST_1"]
+
+  verify_ssl                = true
+  treat_redirect_as_failure = true
+  bypass_head_request       = true
+
+  custom_header {
+    name  = "Cache-Control"
+    value = "no-cache"
+  }
+  custom_header {
+    name  = "CF-Access-Client-Id"
+    value = var.synthetics_monitor_epgstation_cfa_client_id
+  }
+  custom_header {
+    name  = "CF-Access-Client-Id"
+    value = var.synthetics_monitor_epgstation_cfa_client_secret
+  }
+}
