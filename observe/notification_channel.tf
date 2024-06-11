3d9e6e90a757aa7c1374c304fb8d67bfdb866d4f
Hiroto Kitazawa <hiroxto@gmail.com>
2024-06-11 22:26:46 +0900

    feat: DTV 関連の通知を受け取る Slack チャンネルを追加

--- observe/notification_channel.tf
+++ observe/notification_channel.tf
@@ -10,3 +10,16 @@ resource "newrelic_notification_channel" "slack_general" {
     display_value = "200_newrelic_notification"
   }
 }
+
+resource "newrelic_notification_channel" "slack_dtv_alert" {
+  name           = "200_newrelic_dtv_alert_notification"
+  type           = "SLACK"
+  destination_id = data.newrelic_notification_destination.slack.id
+  product        = "IINT"
+
+  property {
+    key           = "channelId"
+    value         = "C077N1R6T9A"
+    display_value = "200_newrelic_dtv_alert_notification"
+  }
+}
