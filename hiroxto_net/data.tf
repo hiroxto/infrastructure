0443b0e67380fa681f60964955af1fca963ae8ad
Hiroto Kitazawa <hiroxto@gmail.com>
2024-06-16 23:40:14 +0900

    feat: cloudflare_zone_dnssec を Data Sources に切り替え

--- hiroxto_net/data.tf
+++ hiroxto_net/data.tf
@@ -2,3 +2,7 @@ data "cloudflare_zone" "main" {
   account_id = var.cloudflare_account_id
   name       = var.name
 }
+
+data "cloudflare_zone_dnssec" "main" {
+  zone_id = data.cloudflare_zone.main.id
+}
--- hiroxto_net/zone_dnssec.tf
+++
@@ -1,3 +0,0 @@
-resource "cloudflare_zone_dnssec" "main" {
-  zone_id = data.cloudflare_zone.main.id
-}
