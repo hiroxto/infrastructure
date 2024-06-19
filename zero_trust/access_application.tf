aae2e4d11f43ed70f15d4f0e1c627c56ba97b260
Hiroto Kitazawa <hiroxto@gmail.com>
2024-06-20 02:14:47 +0900

    chore: New RelicのSynthetics Monitorを一時的に無効化したのでアクセスポリシーからも一時的に外す

--- zero_trust/access_application.tf
+++ zero_trust/access_application.tf
@@ -9,7 +9,7 @@ resource "cloudflare_access_application" "epgstation" {
   ]
   policies = [
     cloudflare_access_policy.admin.id,
-    cloudflare_access_policy.new_relic.id
+    # cloudflare_access_policy.new_relic.id
   ]
   auto_redirect_to_identity  = false
   session_duration           = "168h" # 1 weeks
