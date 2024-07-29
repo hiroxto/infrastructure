178136875e137977244d86db4aabba3eb045a0f6
Hiroto Kitazawa <hiroxto@gmail.com>
2024-07-29 22:48:03 +0900

    feat: エラーログの通知をEQ12-01だけにした

--- observe/data.tf
+++ observe/data.tf
@@ -1,3 +1,8 @@
 data "newrelic_notification_destination" "slack" {
   id = "c9d379d7-6e75-4351-8c75-7f3d8154e121"
 }
+
+data "newrelic_entity" "eq12_01" {
+  name   = "eq12-01"
+  domain = "INFRA"
+}
