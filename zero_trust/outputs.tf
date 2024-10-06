e125dcd055042b97024462cb0699d822b8ab1762
Hiroto Kitazawa <hiroxto@gmail.com>
2024-10-06 22:28:04 +0900

    feat: Cloudflare Tunnelのリソースを新リソースに移行

--- zero_trust/outputs.tf
+++ zero_trust/outputs.tf
@@ -2,21 +2,21 @@
 # eq12_01
 #
 output "tunnel_eq12_01_id" {
-  value = cloudflare_tunnel.eq12_01.id
+  value = cloudflare_zero_trust_tunnel_cloudflared.eq12_01.id
 }
 
 output "tunnel_eq12_01_secret" {
-  value     = cloudflare_tunnel.eq12_01.secret
+  value     = cloudflare_zero_trust_tunnel_cloudflared.eq12_01.secret
   sensitive = true
 }
 
 output "tunnel_eq12_01_token" {
-  value     = cloudflare_tunnel.eq12_01.tunnel_token
+  value     = cloudflare_zero_trust_tunnel_cloudflared.eq12_01.tunnel_token
   sensitive = true
 }
 
 output "tunnel_eq12_01_cname" {
-  value = cloudflare_tunnel.eq12_01.cname
+  value = cloudflare_zero_trust_tunnel_cloudflared.eq12_01.cname
 }
 
 #
@@ -45,7 +45,7 @@ output "tunnel_raspi_4b_01_cname" {
 # CNAME
 #
 output "tunnel_epgstation_cname" {
-  value = cloudflare_tunnel.eq12_01.cname
+  value = cloudflare_zero_trust_tunnel_cloudflared.eq12_01.cname
 }
 
 #
--- zero_trust/tunnel.tf
+++ zero_trust/tunnel.tf
@@ -1,7 +1,7 @@
 #
 # eq12-01
 #
-resource "cloudflare_tunnel" "eq12_01" {
+resource "cloudflare_zero_trust_tunnel_cloudflared" "eq12_01" {
   account_id = var.cloudflare_account_id
   name       = "eq12_01"
   secret     = base64encode(random_password.eq12_01_tunnel_secret.result)
@@ -12,9 +12,9 @@ resource "random_password" "eq12_01_tunnel_secret" {
   length = 64
 }
 
-resource "cloudflare_tunnel_config" "eq12_01" {
+resource "cloudflare_zero_trust_tunnel_cloudflared_config" "eq12_01" {
   account_id = var.cloudflare_account_id
-  tunnel_id  = cloudflare_tunnel.eq12_01.id
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.eq12_01.id
 
   config {
     ingress_rule {
