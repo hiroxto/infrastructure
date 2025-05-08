a541383da659f7c46e689669da6caf74c49c1d41
Hiroto Kitazawa <hiroxto@gmail.com>
2025-05-09 00:01:36 +0900

    fix: cloudflare_dns_recordのnameにドメイン全体を入れる

--- hiroxto_net/outputs.tf
+++ hiroxto_net/outputs.tf
@@ -3,13 +3,13 @@ output "zone_id" {
 }
 
 output "cname_eq12_01_ssh_hostname" {
-  value = "${cloudflare_dns_record.cname_eq12_01_ssh.name}.${data.cloudflare_zone.main.name}"
+  value = cloudflare_dns_record.cname_eq12_01_ssh.name
 }
 
 output "cname_raspi_4b_01_ssh_hostname" {
-  value = "${cloudflare_dns_record.cname_raspi_4b_01_ssh.name}.${data.cloudflare_zone.main.name}"
+  value = cloudflare_dns_record.cname_raspi_4b_01_ssh.name
 }
 
 output "cname_epgstation_hostname" {
-  value = "${cloudflare_dns_record.cname_epgstation.name}.${data.cloudflare_zone.main.name}"
+  value = cloudflare_dns_record.cname_epgstation.name
 }
