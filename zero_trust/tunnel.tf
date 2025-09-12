6e2ce54d8d79e8eb92f7bed623bbba085bc6d297
Hiroto Kitazawa <hiroxto@gmail.com>
2025-09-12 23:26:26 +0900

    feat: ルーティングの追加漏れ対応

--- zero_trust/tunnel.tf
+++ zero_trust/tunnel.tf
@@ -29,6 +29,24 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "eq12_01" {
         }
       },
       {
+        hostname = cloudflare_zero_trust_access_application.fr24_feeder_status.domain
+        service  = "http://127.0.0.1:8080"
+        origin_request = {
+          connect_timeout = 60
+          tls_timeout     = 60
+          tcp_keep_alive  = 60
+        }
+      },
+      {
+        hostname = cloudflare_zero_trust_access_application.piaware.domain
+        service  = "http://127.0.0.1:8080"
+        origin_request = {
+          connect_timeout = 60
+          tls_timeout     = 60
+          tcp_keep_alive  = 60
+        }
+      },
+      {
         hostname = cloudflare_zero_trust_access_application.eq12_01_ssh.domain
         service  = "ssh://127.0.0.1:9922"
       },
