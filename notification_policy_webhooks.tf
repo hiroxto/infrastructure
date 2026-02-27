5ac129c85f93c0fd977ce81f97561df4cf5e7e11
Hiroto Kitazawa <hiroxto@gmail.com>
2026-02-27 20:51:27 +0900

    chore: 通知系のリソースをルートに持ってくる

--- notification_policy_webhooks.tf
+++ notification_policy_webhooks.tf
@@ -1,8 +1,8 @@
 resource "cloudflare_notification_policy_webhooks" "slack" {
   account_id = var.cloudflare_account_id
   name       = "Cloudflare Notification"
-  url        = var.slack_general_webhook_url
-  secret     = var.slack_general_webhook_secret
+  url        = var.cf_slack_general_webhook_url
+  secret     = var.cf_slack_general_webhook_secret
 
   lifecycle {
     ignore_changes = [
@@ -16,8 +16,8 @@ resource "cloudflare_notification_policy_webhooks" "slack" {
 resource "cloudflare_notification_policy_webhooks" "slack_tunnel" {
   account_id = var.cloudflare_account_id
   name       = "Cloudflare Tunnel event"
-  url        = var.slack_tunnel_webhook_url
-  secret     = var.slack_tunnel_webhook_secret
+  url        = var.cf_slack_tunnel_webhook_url
+  secret     = var.cf_slack_tunnel_webhook_secret
 
   lifecycle {
     ignore_changes = [
@@ -31,8 +31,8 @@ resource "cloudflare_notification_policy_webhooks" "slack_tunnel" {
 resource "cloudflare_notification_policy_webhooks" "slack_status" {
   account_id = var.cloudflare_account_id
   name       = "Cloudflare Status"
-  url        = var.slack_status_webhook_url
-  secret     = var.slack_status_webhook_secret
+  url        = var.cf_slack_status_webhook_url
+  secret     = var.cf_slack_status_webhook_secret
 
   lifecycle {
     ignore_changes = [
@@ -46,8 +46,8 @@ resource "cloudflare_notification_policy_webhooks" "slack_status" {
 resource "cloudflare_notification_policy_webhooks" "slack_pages_prod" {
   account_id = var.cloudflare_account_id
   name       = "Cloudflare Pages Production deployment"
-  url        = var.slack_pages_prod_webhook_url
-  secret     = var.slack_pages_prod_webhook_secret
+  url        = var.cf_slack_pages_prod_webhook_url
+  secret     = var.cf_slack_pages_prod_webhook_secret
 
   lifecycle {
     ignore_changes = [
@@ -61,8 +61,8 @@ resource "cloudflare_notification_policy_webhooks" "slack_pages_prod" {
 resource "cloudflare_notification_policy_webhooks" "slack_pages_preview" {
   account_id = var.cloudflare_account_id
   name       = "Cloudflare Pages Preview deployment"
-  url        = var.slack_pages_preview_webhook_url
-  secret     = var.slack_pages_preview_webhook_secret
+  url        = var.cf_slack_pages_preview_webhook_url
+  secret     = var.cf_slack_pages_preview_webhook_secret
 
   lifecycle {
     ignore_changes = [
