f2e57e3296ebf9bd982daa678de5c64ceab8ba82
Hiroto Kitazawa <hiroxto@gmail.com>
2026-04-03 22:43:36 +0900

    feat: 削除予定のCloudflare Pagesプロジェクトを通知対象から削除

--- data.tf
+++ data.tf
@@ -5,24 +5,9 @@ data "cloudflare_zone" "hiroxto_net" {
   zone_id = "1c68e572f5d9da0eeb2147cedd370816"
 }
 
-data "cloudflare_pages_project" "swarm_checkin_regulation_checker" {
-  account_id   = var.cloudflare_account_id
-  project_name = "swarm-checkin-regulation-checker"
-}
-
 data "cloudflare_pages_project" "train_photo_blog" {
   account_id   = var.cloudflare_account_id
   project_name = "hiroxto-train-photo-blog"
-}
-
-data "cloudflare_pages_project" "utils" {
-  account_id   = var.cloudflare_account_id
-  project_name = "hiroxto-utils-hiroxto-net"
-}
-
-data "cloudflare_pages_project" "fare_ticket_route_planning" {
-  account_id   = var.cloudflare_account_id
-  project_name = "fare-ticket-route-planning"
 }
 
 data "cloudflare_zero_trust_tunnel_cloudflared_token" "eq12_01" {
